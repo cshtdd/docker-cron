5ba68ac0586629b27702d0e614d4b65ff7a7fbb4
camilin87 <camilin87@gmail.com>
2017-02-08 14:20:22 -0500

    command to pull an image

--- pull-image.js
+++ pull-image.js
@@ -1,11 +1,19 @@
 var curl = require('curlrequest')
 
-
 console.log("Downloading image")
+
+var imageName = process.argv[2] || ""
+
+if (!imageName.length){
+    throw new Error("imageName argument missing")
+}
+
+console.log("ImageName", imageName)
+
 
 curl.request({
     "unix-socket": "/var/run/docker.sock",
-    url: 'http:/v1.25/images/create?fromImage=node:4',
+    url: 'http:/v1.25/images/create?fromImage=' + imageName,
     method: "POST",
     headers: {"Content-Type": "application/json"},
     include: true
