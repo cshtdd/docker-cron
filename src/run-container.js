f6e3ed9ec6817627d6f125bd59e71cf34401d2ca
camilin87 <camilin87@gmail.com>
2017-02-08 17:16:42 -0500

    get the container Id of the created container

--- src/run-container.js
+++ src/run-container.js
@@ -16,12 +16,16 @@ curl.request({
     url: 'http:/v1.26/containers/create?name=' + 'testnginxcontainer',
     method: "POST",
     headers: {"Content-Type": "application/json"},
-    include: true,
     data: imageConfiguration
 }, function(err, parts) {
     if (err) throw err;
     console.log("RESPONSE")
     console.log(parts)
+
+    var containerInfo = JSON.parse(parts)
+    var containerId = containerInfo.Id
+    // console.log(containerInfo)
+    console.log(containerId)
 
     console.log("Completed")
 })
