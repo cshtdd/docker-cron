5690467073f4beb20347d1115d48d54a7dce905d
camilin87 <camilin87@gmail.com>
2017-02-09 20:35:26 -0500

    extract the run environment propagation tests

--- spec/run_spec.rb
+++ spec/run_spec.rb
@@ -36,39 +36,4 @@ describe "run" do
 
         expect(`docker ps`).to include(@container_name)
     end
-
-    describe "propagates environment variables" do
-        def delete_environment_file()
-            `rm -f src/.env`
-        end
-
-        def create_environment_variable(name, value)
-            
-        end
-
-        before do
-            delete_environment_file()
-
-            @env_var_name = "random_var"
-            @env_var_value = "value_test_#{rand(100000)}"
-            create_environment_variable(@env_var_name, @env_var_value)
-        end
-
-        after do
-            delete_environment_file()
-        end
-
-        it "propagates variables from the environment file" do
-
-        end
-
-        it "maintains variables from the container definition" do
-        end
-
-        it "does not propagate variables that are not in the environment file" do
-        end
-
-        it "overwrites variables from the container definition" do
-        end
-    end
 end
