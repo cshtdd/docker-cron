ebe6019767dc3a91b0c2f015a8b0f8445a875281
Camilo Sanchez <camilin87@gmail.com>
2017-02-11 12:49:16 -0500

    briefly describe how to test the cron command

---
+++ spec/cron_spec.rb
@@ -0,0 +1,10 @@
+require 'utils'
+
+describe "cron" do
+
+    # generate environment file
+    # generate src/.containerInfo.json
+    # sh "rake run[cron,'#{build_container_info_arg container_info}']"
+    # not sure how to validate the logs
+
+end
