0987c22825f04a75a719dc5952ba0f5cf1ed6da7
Camilo Sanchez <camilin87@gmail.com>
2017-02-11 13:00:24 -0500

    now I know how to test the sucker

--- spec/cron_spec.rb
+++ spec/cron_spec.rb
@@ -2,10 +2,17 @@ require 'utils'
 
 describe "cron" do
     CONTAINER_INFO_FILE_NAME = "containerInfo.json"
+    CRON_LOG_FILE_NAME = "cron.log"
 
     def delete_container_info_file
         Dir.chdir('src') do
             `rm -f #{CONTAINER_INFO_FILE_NAME}`
+        end
+    end
+
+    def delete_cron_log
+        Dir.chdir('src') do
+            `rm -f #{CRON_LOG_FILE_NAME}`
         end
     end
 
@@ -21,15 +28,19 @@ describe "cron" do
 
     before do
         delete_container_info_file
+        delete_cron_log
     end
 
     after do
         delete_container_info_file
+        delete_cron_log
     end
 
     # generate environment file
     # generate src/.containerInfo.json
     # sh "rake run[cron,'#{build_container_info_arg container_info}']"
     # not sure how to validate the logs
+    #   make the cron container write printenv VAR1 to /usr/src/app/cron.log
+    #   validate src/cron.log
 
 end
