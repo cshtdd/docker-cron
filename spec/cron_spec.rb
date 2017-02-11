a0ebe837eb2d6145543f26b4138dae8212f6f799
Camilo Sanchez <camilin87@gmail.com>
2017-02-11 12:54:28 -0500

    enhance the cron tests utilities

--- spec/cron_spec.rb
+++ spec/cron_spec.rb
@@ -1,6 +1,31 @@
 require 'utils'
 
 describe "cron" do
+    CONTAINER_INFO_FILE_NAME = "containerInfo.json"
+
+    def delete_container_info_file
+        Dir.chdir('src') do
+            `rm -f #{CONTAINER_INFO_FILE_NAME}`
+        end
+    end
+
+    def run(container_info)
+        Dir.chdir('src') do
+            File.open(CONTAINER_INFO_FILE_NAME, 'w') do |file|
+                file.write(container_info)
+            end
+        end
+
+        sh "rake run[cron]"
+    end
+
+    before do
+        delete_container_info_file
+    end
+
+    after do
+        delete_container_info_file
+    end
 
     # generate environment file
     # generate src/.containerInfo.json
