3526558eb7d0ef5a0357e8179b00277804238aa7
Camilo Sanchez <camilin87@gmail.com>
2017-02-11 13:08:03 -0500

    i have a failing test woo hoo

--- spec/cron_spec.rb
+++ spec/cron_spec.rb
@@ -16,7 +16,7 @@ describe "cron" do
         end
     end
 
-    def run(container_info)
+    def cron(container_info)
         Dir.chdir('src') do
             File.open(CONTAINER_INFO_FILE_NAME, 'w') do |file|
                 file.write(container_info)
@@ -27,6 +27,8 @@ describe "cron" do
     end
 
     before do
+        @env_var_value = "value_test_#{rand(100000)}"
+
         delete_container_info_file
         delete_cron_log
     end
@@ -43,4 +45,24 @@ describe "cron" do
     #   make the cron container write printenv VAR1 to /usr/src/app/cron.log
     #   validate src/cron.log
 
+    it "runs the container" do
+        Env.create_environment_variable("VAR1", @env_var_value)
+        Env.create_environment_variable(COPY_ENV_VARS_SETTING, "VAR1")
+
+        cron %{
+            {
+                "Image": "ubuntu",
+                "Cmd": [
+                    "printenv",
+                    "VAR1",
+                    ">",
+                    "/usr/src/app/#{CRON_LOG_FILE_NAME}"
+                ]
+            }
+        }
+
+        Dir.chdir('src') do
+            expect(`cat #{CRON_LOG_FILE_NAME}`).to include(@env_var_value)
+        end
+    end
 end
