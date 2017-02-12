29a40e85b4d6d2586b53644cc85359978f31183f
Camilo Sanchez <camilin87@gmail.com>
2017-02-12 15:20:29 -0500

    expected pull failure tests don’t delete the alpine image preemptively

--- spec/pull_spec.rb
+++ spec/pull_spec.rb
@@ -1,28 +1,32 @@
 require 'utils'
 
 describe "pull" do
-    before do
-        sh "docker rmi alpine:latest" if `docker image ls alpine:latest`.include?("alpine")
-        expect(`docker image ls alpine:latest`).not_to include("alpine")
+    describe "success" do
+        before do
+            sh "docker rmi alpine:latest" if `docker image ls alpine:latest`.include?("alpine")
+            expect(`docker image ls alpine:latest`).not_to include("alpine")
+        end
+
+        it "pulls a non-existent image" do
+            sh 'rake run[pull,"alpine"]'
+            expect(`docker image ls alpine:latest`).to include("alpine")
+        end
+
+        it "pulls an existent image" do
+            sh 'rake run[pull,"alpine"]'
+            sh 'rake run[pull,"alpine"]'
+
+            expect(`docker image ls alpine:latest`).to include("alpine")
+        end
     end
 
-    it "pulls a non-existent image" do
-        sh 'rake run[pull,"alpine"]'
-        expect(`docker image ls alpine:latest`).to include("alpine")
-    end
+    describe "error" do
+        it "fails pulling an unknown image" do
+            expect(`rake run[pull,"camilin87/notfound"]`).to include("camilin87/notfound not found")
+        end
 
-    it "pulls an existent image" do
-        sh 'rake run[pull,"alpine"]'
-        sh 'rake run[pull,"alpine"]'
-
-        expect(`docker image ls alpine:latest`).to include("alpine")
-    end
-
-    it "fails pulling an unknown image" do
-        expect(`rake run[pull,"camilin87/notfound"]`).to include("camilin87/notfound not found")
-    end
-
-    it "fails when no image is specified" do
-        expect(`rake run[pull]`).to include("Error")
+        it "fails when no image is specified" do
+            expect(`rake run[pull]`).to include("Error")
+        end
     end
 end
